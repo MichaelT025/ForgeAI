ad3be3fa0d087a1a06733fcd00af2ee10d387ae0
Michael Tesfaye <michaelkindie14@gmail.com>
2025-12-05 13:29:06 -0600

    Begin reimplementation

--- README.md
+++
@@ -1,2 +0,0 @@
-# AISolidWorks
-Prompt → SolidWorks macro → Execute via Electron (MVP). Supports template mode; LLM mode coming next.
--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('AISW', {
-  previewFromPrompt: (prompt) => ipcRenderer.invoke('preview:fromPrompt', prompt),
-  runFromPrompt: (prompt) => ipcRenderer.invoke('run:fromPrompt', prompt),
-  onLog: (cb) => ipcRenderer.on('runner:log', (_e, msg) => cb(msg)),
-  onError: (cb) => ipcRenderer.on('runner:error', (_e, msg) => cb(msg)),
-  onDone: (cb) => ipcRenderer.on('runner:done', (_e, msg) => cb(msg)),
-});
